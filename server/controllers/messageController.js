dce4613e39fdb7e99e4be057732042a6c7351958
Clare Cerullo <jewelofsong@gmail.com>
2022-04-09 11:30:23 -0400

    server files created

---
+++ server/controllers/messageController.js
@@ -0,0 +1,22 @@
+//import/require database
+
+
+const messageController = {};
+
+//Middleware for posting a new emote/message
+messageController.postMessage = (req, res, next) => {
+
+}
+
+//Middleware for getting emotes/messages
+messageController.getMessages = (req, res, next) => {
+
+}
+
+//Middleware for deleting emote/message
+messageController.deleteMessage = (req, res, next) => {
+
+}
+
+//Export controller
+module.exports = messageController;
